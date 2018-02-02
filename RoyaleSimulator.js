53f9fa598fff2a8a68384d474a54a21ce1ec1ad0
Alex Sjoberg <asjoberg22@yahoo.com>
2018-02-02 18:05:03 -0500

    Initial Commit - Made a small node script to simulate a BR match

---
+++ RoyaleSimulator.js
@@ -0,0 +1,114 @@
+var LivingPlayerList;
+var EncounterThreshold = 75;
+var LootThreshold = 30;
+
+
+var RunSimulation = function(){
+    while(LivingPlayerList.length > 1){
+        RunGameTurn();
+    }
+    console.info("Winner Winner Chicken Dinner!\nCongratulations "+ LivingPlayerList[0].Name + "!")
+}
+
+var RunGameTurn = function(){
+    var unpairedPlayers = LivingPlayerList.slice();
+    var possibleEncounterPairs = [];
+
+    for(var i = 0; i <= unpairedPlayers.length / 2; i++){
+        var player1 = unpairedPlayers.splice(Math.floor(Math.random()*unpairedPlayers.length), 1)[0];
+        var player2 = unpairedPlayers.splice(Math.floor(Math.random()*unpairedPlayers.length), 1)[0];
+         
+        possibleEncounterPairs.push({
+            Player1: player1, 
+            Player2: player2
+        })
+    }
+
+    for(var i = 0; i < possibleEncounterPairs.length; i++){
+        var encounterRoll = Math.floor(Math.random()*100)
+        var Player1 = possibleEncounterPairs[i].Player1;
+        var Player2 = possibleEncounterPairs[i].Player2;
+        if(encounterRoll > EncounterThreshold){
+            console.info(Player1.Name + " is fighting " + Player2.Name+ "!")
+            ProcessEncounter(Player1, Player2)
+        }else{
+            //Didn't fight. Loot environment
+            ProcessPassiveLooting(Player1)
+            ProcessPassiveLooting(Player2)
+        }
+    }
+
+    //Extra unpaired players loot environment
+    for(var i = 0; i < unpairedPlayers.length; i++){
+        ProcessPassiveLooting(unpairedPlayers[i]);
+    }
+
+}
+
+var ProcessEncounter = function(player1, player2){
+    var winner;
+    var loser;
+
+    var player1FightRoll = player1.RollForFight();
+    var player2FightRoll = player2.RollForFight();
+
+    if(player2FightRoll > player1FightRoll){
+        winner = player2;
+        loser = player1;
+    }else{
+        winner = player1;
+        loser = player2;
+    }
+
+    ProcessFightWinner(winner);
+    ProcessFightLoser(loser);
+}
+
+var ProcessFightWinner = function(winningPlayer){
+    winningPlayer.Power += 20; //TODO make this be based on dead player's power
+}
+
+var ProcessFightLoser = function(losingPlayer){
+    var loserIndex = LivingPlayerList.indexOf(losingPlayer);
+    LivingPlayerList.splice(loserIndex, 1);
+    console.info(losingPlayer.Name + " is dead!\n Remaining Players:\n" + LivingPlayerList.map(player => " " + player.Name + " Power - " + player.Power))
+}
+
+var ProcessPassiveLooting = function(lootingPlayer){
+    var playerLootRoll = Math.floor(Math.random() * 100);
+
+    if(playerLootRoll > LootThreshold){
+        console.info(lootingPlayer.Name + " got some loot!")
+        lootingPlayer.Power += 10;
+    }
+
+}
+
+var Player = function(playerName){
+    var self = this;
+
+    self = {
+        Name : playerName,
+        Power : 100,
+        RollForFight : function(){
+            return Math.floor(Math.random) * self.Power;
+        }
+    }
+
+
+    return self;
+}
+
+var InitPlayerList = function(){
+    LivingPlayerList = [
+    new Player("Alex"),
+    new Player("Adam"),
+    new Player("Dave"),
+    new Player("Paul"),
+    new Player("Patrick"),
+    new Player("YaoZhingPing"),
+];
+}
+
+InitPlayerList();
+RunSimulation();
